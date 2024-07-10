527cc7da32d818efc0d71c3f9bb97be3a6ed0e32
SoundaryaDeshpande <98644073+SoundaryaDeshpande@users.noreply.github.com>
2024-07-10 18:45:50 +0530

    SoundaryaD_c9

--- StringCalculator.py
+++ StringCalculator.py
@@ -2,7 +2,9 @@ def add(var1): #test case 1
   if (var1== "" or var1 == "0"):
     return 0
   elif("var2,var3"):
-    return var2+var3
+    x=var2
+    y=var3
+    return x+y
   else:
     return 
     
