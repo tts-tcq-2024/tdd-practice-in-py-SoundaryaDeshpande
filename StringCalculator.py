eeaa4103f262aaae628bab91d05da2b0152b571e
SoundaryaDeshpande <98644073+SoundaryaDeshpande@users.noreply.github.com>
2024-07-11 11:33:10 +0530

    SoundaryaD_c10

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,10 +1,12 @@
 def add(var1): #test case 1
   if (var1== "" or var1 == "0"):
     return 0
-  elif("var2,var3"):
-    x=var2
-    y=var3
-    return x+y
+  elif("var1"):
+    var2=var1.split(',')
+    num1=int(var2[0].strip())
+    num2=int(var2[1].strip())
+    res=num1+num2
+    return res
   else:
     return 
     
