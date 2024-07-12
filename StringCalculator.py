7d096485763cd7bad3d09adb138ccfd981502399
SoundaryaDeshpande <98644073+SoundaryaDeshpande@users.noreply.github.com>
2024-07-12 11:04:50 +0530

    SoundaryaD_c11

--- StringCalculator.py
+++ StringCalculator.py
@@ -5,6 +5,10 @@ def add(var1): #test case 1
     var2=var1.split(',')
     num1=int(var2[0].strip())
     num2=int(var2[1].strip())
+    if(num1>1000):
+      num1=0
+    if(num2>1000):
+      num2=0
     res=num1+num2
     return res
   else:
