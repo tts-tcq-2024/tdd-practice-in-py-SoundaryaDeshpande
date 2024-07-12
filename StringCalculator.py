cc2f429e072f4c9473edfd7d2396197a2ec9552b
SoundaryaDeshpande <98644073+SoundaryaDeshpande@users.noreply.github.com>
2024-07-12 16:18:40 +0530

    SoundaryaD_c21

--- StringCalculator.py
+++ StringCalculator.py
@@ -6,7 +6,7 @@ def add(var1):
     if var1.startswith("//"):
         delimiter = var1[2]  
         var1 = var1[4:]      
-    var2 = re.split(rf"[{delimiter}\n]", var1)
+    var2 = re.split(rf"{re.escape(delimiter)}|\n", var1)
     result = sum(int(num) for num in var2 if num.isdigit() and int(num) <= 1000)
     
     return result
