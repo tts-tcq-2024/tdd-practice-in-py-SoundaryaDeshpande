2cb834c281dbae93f88666df29c697b3f16f6817
SoundaryaDeshpande <98644073+SoundaryaDeshpande@users.noreply.github.com>
2024-07-12 14:07:41 +0530

    SoundaryaD_c18

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,27 +1,22 @@
 import re
 
 def add(var1): 
-    if var1 == "" or var1 == "0":  # Test case 1 and Test case 2
+    if var1 == "" or var1 == "0":
         return 0
     
     delimiter = ','  # Default delimiter
     
-    if var1.startswith("//"):   # Test case 5 and Test case 6
+    if var1.startswith("//"):
         delimiter_check = re.match(r"//(.+)\n", var1)
         if delimiter_check:
             delimiter = delimiter_check.group(1)
             var1 = var1.replace(f"//{delimiter}\n", "", 1)
-        
-        var2 = re.split(r"\n|,", var1)
-        result = sum(int(num) for num in var2 if num.strip().isdigit())
-    else:  # Test case 3 and Test case 4
-        var2 = var1.split(',')
-        Num1 = int(var2[0].strip())
-        Num2 = int(var2[1].strip())
-        if Num1 > 1000:
-            Num1 = 0
-        if Num2 > 1000:
-            Num2 = 0
-        result = Num1 + Num2
+    var2 = re.split(rf"[{delimiter}\n]", var1)
+    result = 0
+    for num in var2:
+        if num.strip().isdigit():
+            num_int = int(num.strip())
+            if num_int <= 1000:
+                result += num_int
     
     return result
