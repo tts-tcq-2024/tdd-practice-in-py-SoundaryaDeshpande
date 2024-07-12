3a8f964b7002c4e2953a027129035ca838bb6021
SoundaryaDeshpande <98644073+SoundaryaDeshpande@users.noreply.github.com>
2024-07-12 13:49:06 +0530

    SoundaryaD_c15

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,19 +1,19 @@
 import re
 
-def add(var1):
-    if var1 == "" or var1 == "0":
+def add(var1): 
+    if var1 == "" or var1 == "0":  #testcase1 and testcase2
         return 0
-    elif var1.startswith("//"):
-        delimiter = ','
+    delimiter = ','
+    elif var1.startswith("//"):   #testcas5 and tescase6
         delimiter_check = re.match(r"//(.+)\n", var1)
         if delimiter_check:
             delimiter = delimiter_check.group(1)
             var1 = var1.replace(f"//{delimiter}\n", "", 1)
-        var1 = re.sub(f"[{delimiter}\n]", ",", var1)
-        var2 = var1.split(',')
-        var2 = [int(num) for num in var2 if num]
-        result = sum(var2)
-    else:
+        #var1 = re.sub(f"[{delimiter}\n]", ",", var1)
+        var2 = re.split(r"\n|,",var1)
+        #var2 = [int(num) for num in var2 if num]
+        result = sum(int(num) for num in var2 if num.strip().isdigit())
+    else: #testcase3 and testcase4
         var2 = var1.split(',')
         Num1 = int(var2[0].strip())
         Num2 = int(var2[1].strip())
