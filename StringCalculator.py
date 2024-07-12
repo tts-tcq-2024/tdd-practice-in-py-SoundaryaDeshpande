f607e545785418ca3b2a0d14486f15f7417a88e5
SoundaryaDeshpande <98644073+SoundaryaDeshpande@users.noreply.github.com>
2024-07-12 13:13:09 +0530

    SoundaryaD_c14

--- StringCalculator.py
+++ StringCalculator.py
@@ -5,24 +5,28 @@ def add(var1): #test case 1 and test case2
     return 0
   elif("var1"): #test case5 
     if var1.startswith("//"):
-      delimiter=','
-      delimiter_check=re.match(r"//(.+)\n", var1)
-      if delimiter_check:
-        delimiter=delimiter_check.group(1)
-        var1=var1.replace(f"//{delimiter}\n", "", 1)
-    var1=re.sub(f"[{delimiter}\n]", ",",var1)
-    var2=var1.split(',')
-    var2=[int(num) for num in var2 if num]
-    result=sum(var2)
-    elif("var1"): #testcase3 and testcase4
-      var2=var1.split(',')
-      Num1=int(var2[0].strip())
-      Num2=int(var2[1].strp())
-      if(Num1>1000):
-        Num1=0
-      if(Num2>1000):
-        Num2=0
+      {
+        delimiter=','
+        delimiter_check=re.match(r"//(.+)\n", var1)
+        if delimiter_check:
+          delimiter=delimiter_check.group(1)
+          var1=var1.replace(f"//{delimiter}\n", "", 1)
+        var1=re.sub(f"[{delimiter}\n]", ",",var1)
+        var2=var1.split(',')
+        var2=[int(num) for num in var2 if num]
+        result=sum(var2)
+      }
+    if("var1"): #testcase3 and testcase4
+      {
+        var2=var1.split(',')
+        Num1=int(var2[0].strip())
+        Num2=int(var2[1].strp())
+        if(Num1>1000):
+          Num1=0
+        if(Num2>1000):
+          Num2=0
       result=Num1+Num2
+      }
   return result
   else:
     return 
