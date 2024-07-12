192c9b01b893d7f78835f62a29feefbc8a96d6ce
SoundaryaDeshpande <98644073+SoundaryaDeshpande@users.noreply.github.com>
2024-07-12 11:02:15 +0530

    SoundaryaD_Testcase4

--- StringCalculatorTest.py
+++ StringCalculatorTest.py
@@ -8,11 +8,11 @@ class TestStringCalculator(unittest.TestCase):
         def test_expectZeroForSingleZero(self): #Testcase2
                 self.assertEqual(add("0"), 0)
                 
-        def test_expectSumForTwoNumberst(self):#Testcase3
+        def test_expectSumForTwoNumberst(self): #Testcase3
                 self.assertEqual(add("1,2"), 3)
                 
-       # def test_ignoreNumbersGreaterThan1000(self):
-        #        self.assertEqual(add("1,1001"), 1)
+        def test_ignoreNumbersGreaterThan1000(self): #Testcase4
+                self.assertEqual(add("1,1001"), 1)
                 
        # def test_expectSumWithCustomDelimiter(self):
         #        self.assertEqual(add("//;\n1;2"), 3)
