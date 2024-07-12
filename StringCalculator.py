0d58eddce626404f67063aa055890f3f149d270c
SoundaryaDeshpande <98644073+SoundaryaDeshpande@users.noreply.github.com>
2024-07-12 16:39:26 +0530

    SoundaryaD_C22

--- StringCalculator.py
+++ StringCalculator.py
@@ -3,7 +3,7 @@ def add(var1):
     if var1 == "" or var1 == "0":
         return 0
     delimiter = ','  
-    if var1.startswith("//"):
+    else var1.startswith("//"):
         delimiter = var1[2]  
         var1 = var1[4:]      
     var2 = re.split(rf"{re.escape(delimiter)}|\n", var1)
