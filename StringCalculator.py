e197f71edcff9c5dda521bba7a3ab56bdd44c4cf
SoundaryaDeshpande <98644073+SoundaryaDeshpande@users.noreply.github.com>
2024-07-12 17:05:08 +0530

    SoundaryaD_c25

--- StringCalculator.py
+++ StringCalculator.py
@@ -2,10 +2,11 @@ import re
 def add(var1): 
     if var1 == "" or var1 == "0":
         return 0
-    delimiter = ','
-    if var1.startswith("//"):
-        delimiter = re.match(r"//(.+)\n", var1).group(1)
-        var1 = var1.split('\n', 1)[1]  
-    var2 = re.split(f"[{delimiter}\n]", var1)
+    delimiter = ','  
+    else var1.startswith("//"):
+        delimiter = var1[2]  
+        var1 = var1[4:]      
+    var2 = re.split(rf"{re.escape(delimiter)}|\n", var1)
     result = sum(int(num) for num in var2 if num.isdigit() and int(num) <= 1000)
+    
     return result
