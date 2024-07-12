8d8a37111777de1e39ec11904c6291f0b934596e
SoundaryaDeshpande <98644073+SoundaryaDeshpande@users.noreply.github.com>
2024-07-12 16:50:28 +0530

    SoundaryaD_c23

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,5 +1,5 @@
 import re
-def add(var1): 
+def add(var1):
     if var1 == "" or var1 == "0":
         return 0
     delimiter = ','  
@@ -7,5 +7,6 @@ def add(var1):
         delimiter = var1[2]  
         var1 = var1[4:]      
     var2 = re.split(rf"{re.escape(delimiter)}|\n", var1)
-    result = sum(int(num) for num in var2 if num.isdigit() and int(num) <= 1000)
+    var3 = filter(lambda num: num.isdigit() and int(num) <= 1000, var2)
+    result = sum(map(int, var3))
     return result
