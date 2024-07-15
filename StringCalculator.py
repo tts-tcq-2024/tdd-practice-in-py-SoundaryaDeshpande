d9debd35782cbb00f2b1fdf8fcf4cf3be086553d
SoundaryaDeshpande <98644073+SoundaryaDeshpande@users.noreply.github.com>
2024-07-15 12:40:47 +0530

    SoundaryaD_c27

--- StringCalculator.py
+++ StringCalculator.py
@@ -2,10 +2,28 @@ import re
 def add(var1): 
     if var1 == "" or var1 == "0":
         return 0
-    delimiter = ','  
-    if var1.startswith("//"):
-        delimiter = var1[2]  
-        var1 = var1[4:]      
-    var2 = re.split(rf"{re.escape(delimiter)}|\n", var1)
-    result = sum(int(num) for num in var2 if num.isdigit() and int(num) <= 1000)
+    delimiter = delimiter_call(var1)
+    var1=numbers(var1)
+    var2=split_numbers(var1,delimiter)
+    result=valid_number(var2)
     return result
+def delimiter_call(var1):
+     if var1.startswith("//"):
+         return var1[2]
+     else:
+         return ','
+def numbers(var1):
+     if var1.startswith("//"):
+         return var1[4:]
+     else:
+         return var1
+def split_numbers(var1,delimiter):
+    return re.split(rf"{re.escape(delimiter)}|\n", var1)
+def summing_valid_numbers_only(var2):
+    return sum(int(num) for num in var2 if valid_number(num))
+def valid_number(num):
+    return num.isdigit() and int(num) <= 1000
+
+    
+    
+    
